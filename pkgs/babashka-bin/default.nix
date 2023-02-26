65c2725db2bb7bf2781d17b6fcaa0d1b7aed9c9e
Josh Glover <jmglov@gmail.com>
2023-02-26 10:07:55 +0100

    Bump babashka-bin version

--- pkgs/babashka-bin/default.nix
+++ pkgs/babashka-bin/default.nix
@@ -12,25 +12,31 @@ let
     {
       linux = {
         aarch64 =
-          "ffebe85fa9bd3575b885859e47a18a5170e5b25e81b1f80a686144df4b0a29d4";
+          "bc7e733863486b334b8bff83ba13b416800e0ce45050153cb413906b46090d68";
         amd64 =
-          "0bee20da7cd00452d14bfa9c41a5b1362e53cf150897248416950ed60a1f2954";
+          "25975d5424e7dea9fbaef5a6551ce7d3834631b5e28bdc4caf037bf45af57dfd";
       };
       macos = {
-        aarch64 =
-          "763ac565a44e87ac6ef6e6be80a67887260003ad424bf76a1962ed86c5f271ea";
+        # No MacOS builds for ARM at the moment
+        # aarch64 =
+        #   "11c4b4bd0b534db1ecd732b03bc376f8b21bbda0d88cacb4bbe15b8469029123";
         amd64 =
-          "7c1f5dd448ca8c52cfc65b38a5c872d8480c444626ce8ed23efc23b1745ca86e";
+          "792ade86e61703170f3de3082183173db66a9a98b11d01c95ace0235f0a5e345";
       };
     }.${osName}.${arch};
 in stdenv.mkDerivation rec {
   pname = "babashka";
-  version = "1.0.165";
+  version = "1.1.173";
+  filename = if osName == "macos" then
+  # No static builds for MacOS
+    "babashka-${version}-${osName}-${arch}.tar.gz"
+  else
+    "babashka-${version}-${osName}-${arch}-static.tar.gz";
 
   src = builtins.fetchurl {
     inherit sha256;
     url =
-      "https://github.com/babashka/babashka/releases/download/v${version}/babashka-${version}-${osName}-${arch}-static.tar.gz";
+      "https://github.com/babashka/babashka/releases/download/v${version}/${filename}";
   };
 
   dontFixup = true;
