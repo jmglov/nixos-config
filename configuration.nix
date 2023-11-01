e6a8cbb2928535a250e66cb55cf05e654541449c
Josh Glover <jmglov@gmail.net>
2023-11-01 07:20:03 +0100

    Remove deprecated boot.loader.grub.version

--- configuration.nix
+++ configuration.nix
@@ -28,7 +28,6 @@ in {
 
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
   boot.loader.grub.device = "nodev";
   boot.loader.grub.efiSupport = true;
   boot.loader.efi.canTouchEfiVariables = true;
