79752ae530ab0036b569cc0bc848cdda29a43af8
Josh Glover <jmglov@gmail.net>
2024-02-03 19:39:06 +0100

    Move i3 config to home.nix

--- jmglov/home.nix
+++ jmglov/home.nix
@@ -81,12 +81,16 @@ in lib.recursiveUpdate {
       "lithist"
     ];
   };
+
   programs.direnv.enable = true;
+
   programs.firefox.enable = true;
+
   programs.home-manager = {
     enable = true;
     path = "...";
   };
+
   programs.tmux = {
     enable = true;
     prefix = "M-m";
@@ -113,6 +117,76 @@ in lib.recursiveUpdate {
     inactiveInterval = 60;
     lockCmd = "${pkgs.i3lock}/bin/i3lock -n";
   };
+
+  xsession.windowManager.i3 = {
+    enable = true;
+    config = rec {
+      bars = [{
+        hiddenState = "hide";
+        mode = "dock";
+        statusCommand = "${pkgs.i3status}/bin/i3status";
+        trayOutput = "eDP-1";
+      }];
+      keybindings = lib.mkOptionDefault {
+        # Use pactl to adjust volume in PulseAudio.
+        "XF86AudioRaiseVolume" =
+          "exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ +10% && $refresh_i3status";
+        "XF86AudioLowerVolume" =
+          "exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ -10% && $refresh_i3status";
+        "XF86AudioMute" =
+          "exec --no-startup-id pactl set-sink-mute @DEFAULT_SINK@ toggle && $refresh_i3status";
+        "XF86AudioMicMute" =
+          "exec --no-startup-id pactl set-source-mute @DEFAULT_SOURCE@ toggle && $refresh_i3status";
+        # Gotta have screenshots!
+        "Print" = "exec xfce4-screenshooter -f";
+        "Shift+Print" = "exec xfce4-screenshooter -r";
+        "Control+Print" = "exec xfce4-screenshooter -w";
+        # move workspace to next output (monitor)
+        "${modifier}+m" = "move workspace to output next";
+        # Exec mode for extra programs
+        "${modifier}+Shift+Return" = ''mode "exec"'';
+        # Emoji keyboard
+        "${modifier}+period" = "exec rofimoji";
+        # Lock screen
+        "${modifier}+Control+Escape" = "exec i3lock";
+        # Show session dialog
+        "${modifier}+Shift+e" = "exec xfce4-session-logout";
+      };
+      modes = lib.mkOptionDefault {
+        exec = {
+          "b" = "exec brave";
+          "c" = "exec google-chrome-stable";
+          "d" = "exec discord";
+          "e" = "exec emacs";
+          "f" = "exec firefox";
+          "g" = "exec gimp";
+          "i" = "exec idea-community";
+          "k" = "exec kazam";
+          "s" = "exec slack";
+          "v" = "exec protonvpn-app";
+          "x" = "exec xfce4-settings-manager";
+          "z" = "exec zoom";
+          # back to normal: Enter or Escape or $mod+r
+          "Return" = ''mode "default"'';
+          "Escape" = ''mode "default"'';
+          "$mod+Shift+Return" = ''mode "default"'';
+        };
+      };
+      modifier = "Mod4";
+      terminal = "xfce4-terminal";
+      window.border = 0;
+    };
+    extraConfig = ''
+      set $refresh_i3status killall -SIGUSR1 i3status
+      # xss-lock grabs a logind suspend inhibit lock and will use i3lock to lock the
+      # screen before suspend. Use loginctl lock-session to lock your screen.
+      exec --no-startup-id xss-lock --transfer-sleep-lock -- i3lock --nofork
+      # NetworkManager is the most popular way to manage wireless networks on Linux,
+      # and nm-applet is a desktop environment-independent system tray GUI for it.
+      exec --no-startup-id nm-applet
+    '';
+  };
+
 } (if hostName == "alhana" then {
   home.stateVersion = "22.11";
   programs.direnv = {
